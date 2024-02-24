fef7a6a4022644f25b6cef35bffeb9d957d78426
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2024-02-24 11:53:17 +0100

    admin: endpoint for customer orders list

--- app/flaskr/admin.py
+++ app/flaskr/admin.py
@@ -4,7 +4,7 @@ Flask blueprint for logged in Customer views.
 from flask import Blueprint, render_template, g, request, flash, redirect, url_for, session
 from flaskr.auth import login_required
 from flaskr.db.store import get_order_orderlines, get_customer_orders
-from flaskr.db.user import get_user_password, create_user, get_user_by_email, set_user_password, set_user_details, get_all_users
+from flaskr.db.user import get_all_users, get_user_by_id
 from flaskr.store import get_order_total_amount
 import bcrypt
 
@@ -16,3 +16,14 @@ bp = Blueprint("admin", __name__, url_prefix="/admin")
 def customer_list():
     customers = get_all_users('Customer')
     return render_template("admin/customer_list.html", customers=customers)
+
+@bp.route("/customer/<int:id>/orders")
+def customer_orders(id):
+    customer_orders    = get_customer_orders(id)
+    user = get_user_by_id(id, 'Customer')
+    if customer_orders:
+        order_items = [get_order_orderlines(order['id']) for order in customer_orders]
+        total_amounts = [get_order_total_amount(order_item) for order_item in order_items]
+        orders = [{'order': order_item, 'total_amount': total_amount, 'order_date': customer_order['order_date'], 'order_status': customer_order['order_status']} for order_item, total_amount, customer_order in zip(order_items, total_amounts, customer_orders)]
+        return render_template("admin/view_customer_orders.html", orders=orders, user=user)
+    return render_template("admin/view_customer_orders.html", user=user)
